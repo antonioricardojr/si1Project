4af907a6bafd438b7afca48116d8913c3333aab6
Antonio R. Marques Jr. <antonioricardojr@gmail.com>
2012-03-30 17:33:20 -0300

    Alteração na classe Sessao e introdução das classes Si1ProjectFacade e si1ProjectTestClient

---
+++ src/Facade/Si1ProjectTestClient.java
@@ -0,0 +1,30 @@
+package Facade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import easyaccept.EasyAcceptFacade;
+
+public class Si1ProjectTestClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<String> files = new ArrayList<String>();
+		// Put the us1.txt file into the "test scripts" list
+		files.add("US/US01.txt");
+		files.add("US/US02.txt");
+		// Instantiate the Monopoly Game façade
+		Si1ProjectFacade si1ProjectFacade = new Si1ProjectFacade();
+		// Instantiate EasyAccept façade
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(si1ProjectFacade,
+				files);
+
+		eaFacade.executeTests();
+
+		System.out.println(eaFacade.getCompleteResults());
+	}
+
+}
