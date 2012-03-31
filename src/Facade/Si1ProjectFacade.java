bc1e676b9d82ae1f9d1bb1ca8374d7f2d28e4baa
Antonio R. Marques Jr. <antonioricardojr@gmail.com>
2012-03-31 15:31:23 -0300

    teste main Facade

---
+++ src/Facade/Si1ProjectFacade.java
@@ -0,0 +1,13 @@
+package Facade;
+
+public class Si1ProjectFacade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("oiola");
+	}
+
+}
