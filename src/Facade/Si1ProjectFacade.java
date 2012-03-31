10760d2059885561464582050f8cdb8a600928f5
Antonio R. Marques Jr. <antonioricardojr@gmail.com>
2012-03-31 15:34:05 -0300

    Readicionando a Facade

--- src/Facade/Si1ProjectFacade.java
+++ src/Facade/Si1ProjectFacade.java
@@ -1,13 +1,43 @@
-package Facade;
 
+
+
+import si1.logica.Sistema;
+
+
+/**
+ * Classe Facade para usar os testes do easyAccept.
+ * 
+ * @author ANTONIOR
+ *
+ */
 public class Si1ProjectFacade {
-
-	/**
-	 * @param args
-	 */
+	
+	
+	
+	Sistema S1 = new Sistema();	
+		
+	public void zerarSistema(){
+		S1 = new Sistema();
+	}
+	
+	
+	public void criarUsuario(String login, String senha, String nome, String endereco, String email) throws Exception{
+		S1.criarUsuario(login, senha, nome, endereco, email);
+		
+	}
+	
+	public String abrirSessao(String login, String senha) throws Exception{
+		return S1.abrirSessao(login, senha);
+		
+	}
+	
+	
+	public String getAtributoUsuario(String login, String atributo) throws Exception{
+		return S1.getAtributoUsuario(login,atributo);
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("oiola");
 	}
-
-}
+	
---
+++ src/Facade/Si1ProjectTestClient.java
@@ -0,0 +1,33 @@
+package Facade;
+
+
+import Si1ProjectFacade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import easyaccept.EasyAcceptFacade;
+
+public class Si1ProjectTestClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<String> files = new ArrayList<String>();
+		// Put the us1.txt file into the "test scripts" list
+		files.add("US/US01.txt");
+		
+		// Instantiate the Monopoly Game façade
+		Si1ProjectFacade si1ProjectFacade = new Si1ProjectFacade();
+		// Instantiate EasyAccept façade
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(si1ProjectFacade,
+				files);
+
+		eaFacade.executeTests();
+
+		System.out.println(eaFacade.getCompleteResults());
+	}
+
+}
