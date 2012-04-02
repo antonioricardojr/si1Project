39c3ebcd89a9f2fc6bcf3c68300ff1b5539457c9
Caaina Nascimento <caainaje@gmail.com>
2012-04-02 12:25:26 -0300

    Implementação dos Metodos setData implementação do testData

--- src/si1/TesteLogica/CaronaTest.java
+++ src/si1/TesteLogica/CaronaTest.java
@@ -14,6 +14,7 @@ public class CaronaTest {
 	Carona c2;
 	Carona c3;
 	Carona c4;
+	Carona c5;
 
 	@Before
 	public void setUp() throws Exception {
@@ -27,7 +28,7 @@ public class CaronaTest {
 	public void testOrigem() throws Exception {
 		try {
 			c1 = new Carona("091029", "Campina Grande", "Joao Pessoa",
-					"10/02/2012", "19:00", 3);
+					"20/09/2012", "19:00", 3);
 		} catch (Exception e) {
 
 		}
@@ -41,10 +42,18 @@ public class CaronaTest {
 		}
 
 		try {
-			c3 = new Carona("1020301", "", "Fortaleza", "04/06/2012", "09:00", 1);
+			c3 = new Carona("1020301", "", "Fortaleza", "04/08/2012", "09:00", 1);
 		} catch (Exception e) {
 			assertEquals("Origem nula ou invalida", e.getMessage());
 		}
+		
+		try {
+			c4 = new Carona("1020301", "", "Fort@l&z@", "04/08/2012", "09:00", 1);
+		} catch (Exception e) {
+			assertEquals("Origem nula ou invalida", e.getMessage());
+		}
+		
+		
 
 	}
 
@@ -53,26 +62,26 @@ public class CaronaTest {
 
 		try {
 			c1 = new Carona("7894561", "Campina Grande", "Fortaleza",
-					"04/06/2012", "09:00", 1);
+					"04/08/2012", "09:00", 1);
 		} catch (Exception e) {
 		}
 		assertEquals("09:00", c1.getHora());
 
 		try {
 			c2 = new Carona("7894561", "Campina Grande", "Fortaleza",
-					"04/06/2012", "seis", 1);
+					"04/08/2012", "seis", 1);
 		} catch (Exception e) {
 			assertEquals("Hora nula ou invalida.", e.getMessage());
 		}
 		
 		try{
-			c3 = new Carona("7894561", "Campina Grande", "Fortaleza", "04/06/2012", "09000", 1);	
+			c3 = new Carona("7894561", "Campina Grande", "Fortaleza", "04/08/2012", "09000", 1);	
 		}catch(Exception e){
 			assertEquals("Hora nula ou invalida.", e.getMessage());
 		}
 		
 		try{
-			c4 = new Carona("7894561", "Campina Grande", "Fortaleza", "04/06/2012", "009:0", 1);	
+			c4 = new Carona("7894561", "Campina Grande", "Fortaleza", "04/08/2012", "009:0", 1);	
 		}catch(Exception e){
 			assertEquals("Hora nula ou invalida.", e.getMessage());
 		}
@@ -82,7 +91,7 @@ public class CaronaTest {
 	public void testIdSessao(){
 		try {
 			c1 = new Carona("091029", "Campina Grande", "Joao Pessoa",
-					"10/02/2012", "19:00", 3);
+					"10/08/2012", "19:00", 3);
 		} catch (Exception e) {
 
 		}
@@ -90,16 +99,73 @@ public class CaronaTest {
 		
 		try{
 			c2 = new Carona("091029A", "Campina Grande", "Joao Pessoa",
-					"10/02/2012", "19:00", 3);
+					"10/08/2012", "19:00", 3);
 		}catch(Exception e){
 			assertEquals("Id de sessao invalido ou nulo.", e.getMessage());
 		}
 		
 		try{
 			c3 = new Carona("teste", "Campina Grande", "Joao Pessoa",
-					"10/02/2012", "19:00", 3);
+					"10/08/2012", "19:00", 3);
 		}catch(Exception e){
 			assertEquals("Id de sessao invalido ou nulo.", e.getMessage());
 		}
 	}
+
+	@Test
+	public void testData(){
+		try {
+			c1 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"02/08/2012", "19:00", 3);
+		} catch (Exception e) {
+
+		}
+		assertEquals("02/08/2012", c1.getData());
+		
+		try {
+			c2 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"30/02/2013", "19:00", 3);
+		}catch (Exception e){
+			assertEquals("Data nula ou invalida.", e.getMessage());
+		}
+		
+		try {
+			c3 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"29/02/2013", "19:00", 3);
+		}catch (Exception e){
+			assertEquals("Data nula ou invalida.", e.getMessage());
+		}
+		
+		try {
+			c4 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"32/02/2013", "19:00", 3);
+		}catch (Exception e){
+			assertEquals("Data nula ou invalida.", e.getMessage());
+		}
+		
+		try {
+			c5 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"32/02/2010", "19:00", 3);
+		}catch (Exception e){
+			assertEquals("Data nula ou invalida.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testVagas(){
+		try {
+			c1 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"01/02/2013", "19:00", 3);
+		}catch (Exception e){
+		}
+		assertEquals(3, c1.getVagas());
+		
+		try {
+			c2 = new Carona("091029", "Campina Grande", "Joao Pessoa",
+					"01/02/2013", "19:00", -1);
+		}catch (Exception e){
+			assertEquals("Numero de vagas nula ou invalida.", e.getMessage());
+		}
+	}
+
 }
--- src/si1/logica/Carona.java
+++ src/si1/logica/Carona.java
@@ -1,6 +1,7 @@
 package si1.logica;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
@@ -43,7 +44,7 @@ public class Carona {
 	}
 
 	public void setOrigem(String origem) throws Exception {
-		if (origem == null || origem.equals("")) {
+		if (origem == null || origem.equals("") || contemCharInvalidos(origem)) {
 			throw new Exception("Origem nula ou invalida");
 		} else {
 			this.origem = origem;
@@ -55,13 +56,25 @@ public class Carona {
 	}
 
 	public void setDestino(String destino) throws Exception {
-		if (destino == null || destino.equals("")) {
+		if (destino == null || destino.equals("") || contemCharInvalidos(destino)) {
 			throw new Exception("Destino nulo ou invalido.");
 		} else {
 			this.destino = destino;
 		}
 	}
 
+	private static boolean contemCharInvalidos(String nome) {
+		String[] palavra = nome.trim().split("");
+
+		for (int i = 1; i < palavra.length; i++) {
+			if ("'!@#$%¨¨¨&*()+¹²³£¢¬§=[{ª~^]}º;:>,</|´/`0123456789*-".contains(palavra[i])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
 	public String getData() {
 		return data;
 	}
@@ -73,18 +86,98 @@ public class Carona {
 			this.data = data;
 		}
 	}
-	
-	private static boolean validaData(String data){
-		if(data.length()!=10){
+
+	@SuppressWarnings("deprecation")
+	private static boolean validaData(String data) throws Exception {
+		Date date = new Date();
+		int ano;
+		int mes;
+		int dia;
+		if (data.length() != 10) {
 			return false;
 		}
-		String dia = data.substring(0, 2);
-		String mes = data.substring(3, 5);
-		String ano = data.substring(6);
+		try {
+
+			dia = Integer.parseInt(data.substring(0, 2));
+			mes = Integer.parseInt(data.substring(3, 5));
+			ano = Integer.parseInt(data.substring(6));
+
+		} catch (Exception e) {
+			throw new Exception("Data nula ou invalida.");
+		}
+
+		// Verifica ano >= que atual
+		if (ano < date.getYear() + 1900) {
+			return false;
+		}
+
+		// Verifica mes entre 01 e 12
+		if (mes < 1 || mes > 12) {
+			return false;
+		}
+
+		// Verifica para meses com 31 dias
+		if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8
+				|| mes == 10 || mes == 12)
+				&& (dia < 1 || dia > 31)) {
+			return false;
+
+		}
+
+		// Verifica para meses com 30 dias
+		if ((mes == 4 || mes == 6 || mes == 9 || mes == 11)
+				&& (dia < 1 || dia > 30)) {
+			return false;
+
+		}
+
+		// Verifica para fevereiro bissexto com 29 dias
+		if ((mes == 2) && (ano % 4 == 0) && (dia < 1 || dia > 29)) {
+			return false;
+		}
+
+		// Verifica para fevereiro não bissexto com 28 dias
+		if ((mes == 2) && (ano % 4 != 0) && (dia < 1 || dia > 28)) {
+			return false;
+		}
+
+		//Compara com a data atual
+		if(comparaDataAtual(dia, mes, ano)<0){
+			return false;
+		}
 		
-		
+		// Aceita a data
 		return true;
-		
+
+	}
+	
+	private static int comparaDataAtual(int dia, int mes, int ano){
+		Date date = new Date();
+		if(ano==date.getYear()+1900){
+			if(mes==date.getMonth()+1){
+				if(dia==date.getDay()+1){
+					return 0;
+				}else{
+					if(dia>date.getDay()+1){
+						return 1;
+					}else{
+						return -1;
+					}
+				}
+			}else{
+				if(mes>date.getMonth()+1){
+					return 1;
+				}else{
+					return -1;
+				}
+			}
+		}else{
+			if(ano>date.getYear()+1900){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
 	}
 
 	public String getHora() throws Exception {
@@ -105,9 +198,9 @@ public class Carona {
 		} else {
 			String[] caracteres = hora.split("");
 			for (int i = 1; i < caracteres.length; i++) {
-				if (i == 3){
-					if(!caracteres[3].equals(":")) {
-						return false;					
+				if (i == 3) {
+					if (!caracteres[3].equals(":")) {
+						return false;
 					}
 				} else {
 					if (temInvalidoNosNumeros((caracteres[i]))) {
@@ -139,10 +232,10 @@ public class Carona {
 	public void setIdSessao(String idSessao) throws Exception {
 		if (idSessao == null || idSessao.equals("")) {
 			throw new Exception("Id de sessao nulo ou invalido.");
-		}else{
+		} else {
 			String[] caracteres = idSessao.split("");
-			for(int i = 1; i<caracteres.length;i++){
-				if(temInvalidoNosNumeros(caracteres[i])){
+			for (int i = 1; i < caracteres.length; i++) {
+				if (temInvalidoNosNumeros(caracteres[i])) {
 					throw new Exception("Id de sessao invalido ou nulo.");
 				}
 			}
@@ -150,11 +243,15 @@ public class Carona {
 		this.idSessao = idSessao;
 	}
 
-	public static boolean temInvalidoNosNumeros(String s){
-		if(!"0123456789".contains(s)){
+	public static boolean temInvalidoNosNumeros(String s) {
+		if (!"0123456789".contains(s)) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
+	}
+
+	public static void main(String[] args){
+		System.out.println(contemCharInvalidos("bucata oiE"));
 	}
 }
